30468ec11a6568425957cbae1ee8e8c739804234
oussemajaziri <102968825+oussemajaziri@users.noreply.github.com>
2022-05-21 13:18:02 +0100

    Add files via upload
    Dernière étape avant SpringBoot

---
+++ core-spring-vers-10/src/main/java/com/mycompany/invoise/core/App.java
@@ -0,0 +1,40 @@
+package com.mycompany.invoise.core;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import com.mycompany.invoise.core.service.prefix.InvoiceServicePrefix;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+@ComponentScan
+@PropertySource("classpath:application.properties")
+public class App {
+	public static void main(String[] args) {
+		 
+		//ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+		
+		InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
+		
+		
+		
+		invoiceController.createInvoice();
+		
+		
+	}
+	
+}
---
+++ core-spring-vers-10/src/main/java/com/mycompany/invoise/core/controller/scan/InvoiceControllerDouchette.java
@@ -0,0 +1,36 @@
+package com.mycompany.invoise.core.controller.scan;
+
+import java.util.Scanner;
+
+import org.springframework.stereotype.Controller;
+
+import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.core.entity.Invoice;
+import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import com.mycompany.invoise.core.service.number.InvoiceServiceNumber;
+
+//@Controller
+public class InvoiceControllerDouchette implements InvoiceControllerInterface {
+	
+	private InvoiceServiceInterface invoiceService;
+	
+	
+	public InvoiceServiceInterface getInvoiceService() {
+		return invoiceService;
+	}
+
+	public void setInvoiceService(InvoiceServiceInterface invoiceService) {
+		this.invoiceService = invoiceService;
+	}
+
+	@Override
+	public void createInvoice() {
+        System.out.println( "Usage de scanner" );
+ 
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName("Virgin Galactic");
+        
+        invoiceService.createInvoice(invoice);
+	}
+
+}
