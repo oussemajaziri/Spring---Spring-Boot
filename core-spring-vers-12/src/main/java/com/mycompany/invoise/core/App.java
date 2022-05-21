e36fe70f7b1c1fbbf5f400eafdc4ba4ef4f1bd1d
oussemajaziri <102968825+oussemajaziri@users.noreply.github.com>
2022-05-21 14:38:00 +0100

    Add files via upload
    Modif du pom.xml pour rendre notre application une vrai application Spring Boot

---
+++ core-spring-vers-12/src/main/java/com/mycompany/invoise/core/App.java
@@ -0,0 +1,41 @@
+package com.mycompany.invoise.core;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import com.mycompany.invoise.core.service.prefix.InvoiceServicePrefix;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		 
+		//ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+		ApplicationContext context = SpringApplication.run(App.class,args);
+		
+		InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
+		
+		
+		
+		invoiceController.createInvoice();
+		
+		
+	}
+	
+}
